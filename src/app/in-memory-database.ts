c7fc53cf10ea3ffd7356d5973f51190a017cf916
Arthur Inácio <arthur.inacio@zorte.com.br>
2018-12-12 11:07:46 -0200

    adding category model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,8 +1,9 @@
 import { InMemoryDbService } from 'angular-in-memory-web-api';
+import { Category } from './pages/categories/shared/category.model';
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb() {
-        const categories = [
+        const categories: Category[] = [
             {id: 1, name: 'lazer', description: 'Pagamento de conta para casa'},
             {id: 2, name: 'Saúde', description: 'Remedio para gripe'},
             {id: 3, name: 'Salário', description: 'Trabalhos como freelancer'}
